5fbc5962d2a12cec4f700e5fe6c0fd86c232e016
AlexandroMunera <alexandromunera@gmail.com>
2016-11-24 15:40:26 -0500

    Opción scanear pareja

--- BarCodeScanner/BarCodeScanner/HomePage.cs
+++ BarCodeScanner/BarCodeScanner/HomePage.cs
@@ -15,7 +15,18 @@ namespace BarCodeScanner
             Button scanBtn = new Button
             {
                 Text = "Verificar Producto",
-                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand
+
+            };
+
+            Button scanParejaBtn = new Button
+            {
+                Text = "Scanear la pareja",
+                IsVisible = false,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand
+
             };
 
             scanBtn.Clicked += async (sender, args) =>
@@ -23,13 +34,17 @@ namespace BarCodeScanner
                 var scanResult = await Acr.BarCodes.BarCodes.Instance.Read();
                 if (!scanResult.Success)
                 {
-                    await this.DisplayAlert("Alerta ! ", "Perdon ! \n Error al leer el código !", "OK");
+                    await this.DisplayAlert("Alerta ! ", "Error al leer el código, intenta nuevamente", "OK");
                 }
                 else
                 {
                     if (scanResult.Code.ToString() == "Garrafa1")
                     {
                         await this.DisplayAlert("Verificado !", "Este producto esta certificado y proviene de una fuente confiable.", "OK");
+
+
+                        //Habilitar el botón para scanear la pareja
+                        scanParejaBtn.IsVisible = true;
 
                     }
                     else
@@ -40,33 +55,63 @@ namespace BarCodeScanner
                 }
             };
 
-            var layout = new StackLayout();
-
-            var yellowBox = new BoxView {
-                Color = Color.Yellow,
-                VerticalOptions = LayoutOptions.FillAndExpand,
-                HorizontalOptions = LayoutOptions.FillAndExpand
-            };
-
-            var greenBox = new BoxView {
-                Color = Color.Green,
-                VerticalOptions = LayoutOptions.FillAndExpand,
-                HorizontalOptions = LayoutOptions.FillAndExpand
-            };
-
-            var blueBox = new BoxView
+            scanParejaBtn.Clicked += async (sender, args) =>
             {
-                Color = Color.Blue,
-                VerticalOptions = LayoutOptions.FillAndExpand,
-                HorizontalOptions = LayoutOptions.FillAndExpand,
-                HeightRequest = 75
+                var scanResult = await Acr.BarCodes.BarCodes.Instance.Read();
+                if (!scanResult.Success)
+                {
+                    await this.DisplayAlert("Alerta ! ", "Error al leer el código, intenta nuevamente", "OK");
+                }
+                else
+                {
+                    if (scanResult.Code.ToString() == "ParejaGarrafa1")
+                    {
+                        await this.DisplayAlert("Correcto !", "Este producto se ha inhabilitado para un posterior uso, puedes consumirlo tranquilamente, gracias por salvar vidas !", "OK");
+
+                    }
+                    else
+                    {
+                        await this.DisplayAlert("¡ CUIDADO !", "Este código no es la pareja de este producto, ten cuidado si decides consumirlo.", "OK");
+
+                    }
+                }
             };
+
+
+            var layout = new StackLayout { Padding = 80 };
+
+            //var yellowBox = new BoxView
+            //{
+            //    Color = Color.Yellow,
+            //    VerticalOptions = LayoutOptions.FillAndExpand,
+            //    HorizontalOptions = LayoutOptions.FillAndExpand
+            //};
+
+            //var greenBox = new BoxView
+            //{
+            //    Color = Color.Green,
+            //    VerticalOptions = LayoutOptions.FillAndExpand,
+            //    HorizontalOptions = LayoutOptions.FillAndExpand
+            //};
+
+            //var blueBox = new BoxView
+            //{
+            //    Color = Color.Blue,
+            //    VerticalOptions = LayoutOptions.FillAndExpand,
+            //    HorizontalOptions = LayoutOptions.FillAndExpand,
+            //    HeightRequest = 75
+            //};
 
             layout.Children.Add(scanBtn);
-            layout.Children.Add(yellowBox);
-            layout.Children.Add(greenBox);
-            layout.Children.Add(blueBox);
-            layout.Spacing = 10;
+            layout.Children.Add(scanParejaBtn);
+            
+
+            //layout.Children.Add(yellowBox);
+            //layout.Children.Add(greenBox);
+            //layout.Children.Add(blueBox);
+            //layout.Spacing = 10;
+            layout.BackgroundColor = Color.Aqua;
+            
 
             Content = layout;
 
@@ -76,6 +121,33 @@ namespace BarCodeScanner
             //                scanBtn
             //}
             //        };
+
+
+            //var myImage = new Image()
+            //{
+            //    Source = FileImageSource.FromUri(
+            //new Uri("http://xamarin.com/content/images/pages/index/hero-slide.jpg"))
+            //};
+
+            //var myImage = new Image { Aspect = Aspect.AspectFit };
+            //myImage.Source = ImageSource.FromFile("fondo.jpg");
+
+            //RelativeLayout layout = new RelativeLayout();
+
+            //layout.Children.Add(myImage,
+            //    Constraint.Constant(0),
+            //    Constraint.Constant(0),
+            //    Constraint.RelativeToParent((parent) => { return parent.Width; }),
+            //    Constraint.RelativeToParent((parent) => { return parent.Height; }));
+
+            //layout.Children.Add(scanBtn,
+            //    Constraint.Constant(0),
+            //    Constraint.Constant(0),
+            //    Constraint.RelativeToParent((parent) => { return parent.Width; }),
+            //    Constraint.RelativeToParent((parent) => { return parent.Height; }));
+
+
+            //Content = layout;
         }
     }
 }
